c27a10717be1a51f9b39df1da19b6bce97967472
Eric Bréchemier <eric@bezen.org>
2013-12-06 18:15:11 +0100

    Add example to illustrate specific options of sh command
    The specific options correspond to different ways to provide commands
    to run in the shell.

---
+++ examples/sh/specific-options.sh
@@ -0,0 +1,15 @@
+cat <<- END
+	This script illustrates specific options of the sh command,
+	which correspond to different ways to provide commands to run.
+END
+
+command='echo run command'
+
+echo 'Run a command with sh -c command'
+sh -c "$command"
+
+echo 'Run commands provided from standard input with sh -s'
+echo "$command" | sh -s
+
+echo 'Run an interactive shell with sh -i (type exit to quit)'
+sh -i
