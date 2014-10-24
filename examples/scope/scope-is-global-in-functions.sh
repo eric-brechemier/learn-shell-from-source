ac6d3c6dda33ca02b40c906eb847e0ec954102e3
Eric Bréchemier <eric@bezen.org>
2014-10-24 07:07:41 +0200

    Create separate variables at each level to illustrate global scope
    The variables are visible at any level of the call hierarchy.

--- examples/scope/scope-is-global-in-functions.sh
+++ examples/scope/scope-is-global-in-functions.sh
@@ -11,8 +11,13 @@ level1()
   echo "level1: variable is: $variable";
   echo "variable=level1"
   variable=level1;
+  echo "variable1=level1"
+  variable1=level1;
   level2;
   echo "level1: variable is: $variable";
+  echo "level1: variable1 is: $variable1";
+  echo "level1: variable2 is: $variable2";
+  echo "level1: variable3 is: $variable3";
 }
 
 level2()
@@ -20,8 +25,13 @@ level2()
   echo "level2: variable is: $variable";
   echo "variable=level2"
   variable=level2;
+  echo "variable2=level2"
+  variable2=level2;
   level3;
   echo "level2: variable is: $variable";
+  echo "level2: variable1 is: $variable1";
+  echo "level2: variable2 is: $variable2";
+  echo "level2: variable3 is: $variable3";
 }
 
 level3()
@@ -29,7 +39,12 @@ level3()
   echo "level3: variable is: $variable";
   echo "variable=level3"
   variable=level3;
+  echo "variable3=level3"
+  variable3=level3;
   echo "level3: variable is: $variable";
+  echo "level3: variable1 is: $variable1";
+  echo "level3: variable2 is: $variable2";
+  echo "level3: variable3 is: $variable3";
 }
 
 echo "Global: variable is: $variable";
@@ -37,3 +52,6 @@ echo "variable=global"
 variable=global
 level1;
 echo "Global: variable is: $variable";
+echo "Global: variable1 is: $variable1";
+echo "Global: variable2 is: $variable2";
+echo "Global: variable3 is: $variable3";
